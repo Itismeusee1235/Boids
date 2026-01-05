ba34ce14a6075bdf123b9bfe025930a3062ea396
Itismeusee1235 <pradyumnrayasam22@gmail.com>
2026-01-05 09:03:29 +0530

    Cleaned up the code

--- include/boid.hpp
+++ include/boid.hpp
@@ -26,13 +26,13 @@ class Boid {
   const double MAX_RANDOM_STEER = M_PI / 3.0; // In Radians
 
   const double MAX_WANDER_FORCE = 100;
-  const double MAX_AGGREGATION_FORCE = 80;
-  const double MAX_SEGREGATION_FORCE = 200;
+  const double MAX_COHESION_FORCE = 80;
+  const double MAX_SEPARATION_FORCE = 200;
   const double MAX_ALIGNMENT_FORCE = 50;
   const double MAX_AVOIDANCE_FORCE = 400;
 
-  const double AGGREGATION_RADIUS = 100;
-  const double SEGREGATION_RADIUS = 50;
+  const double COHENSION_RADIUS = 100;
+  const double SEPARATION_RADIUS = 50;
   const double ALIGNMENT_RADIUS = 50;
   const double AVOIDANCE_RADIUS = 40;
 
@@ -40,7 +40,7 @@ class Boid {
   const double SEGREGATION_W = 1;
   const double ALIGNMENT_W = 1;
 
-  const double FOV = 4 * M_PI / 3;
+  const double FOV = 2 * M_PI / 3;
   double steeringAngle = 0.0; // In Radians
 
   Boid(int worldWidth, int worldHeight)
@@ -61,6 +61,13 @@ class Boid {
 
   double rotation() { return vel.rotation() * (180.0 / M_PI); }
   void update(double deltaTime, Boid** Boid_List, int n, Object** Objects, int obj_n, SDL_Renderer* renderer);
+
+  Vector Cohesion(Boid** Boid_List, int n);
+  Vector Separation(Boid** Boid_List, int n);
+  Vector Alignement(Boid** Boid_List, int n);
+  Vector ObjectAvoidance(Object** Object_List, int n);
+  Vector Wandering();
+
   void Draw(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect* textRect);
   void print();
 };
--- src/boid.cpp
+++ src/boid.cpp
@@ -3,96 +3,96 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 std::random_device boid_rd;
 std::mt19937 boids_gen(boid_rd());
 std::uniform_real_distribution<double> WanderAngleGen(-1, 1);
 
-void Boid::update(double deltaTime, Boid** Boid_List, int n, Object** Objects, int n_obj, SDL_Renderer* renderer)
+Vector Boid::Cohesion(Boid** Boid_List, int n)
 {
-  Vector steer(0, 0);
-
-  Vector alignmentCentre(0, 0);
-  Vector aggregationCentre(0, 0);
-  int alignCount = 0;
-  int aggCount = 0;
-
-  Vector TotalSegregation(0, 0);
-  Vector TotalAggregation(0, 0);
-  Vector TotalAlignment(0, 0);
-
+  // Returns the force to be applied to velocity
+  Vector sum(0, 0);
+  int count = 0;
   double fov_angle = cos(FOV * 0.5);
 
   for (int i = 0; i < n; i++) {
+    Vector diff = Boid_List[i]->pos - pos;
+    double dist = diff.norm();
+    Vector dir = diff.normalized();
 
-    if (Boid_List[i] == this)
-      continue;
-
-    Vector dist = Boid_List[i]->pos - pos;
-
-    if (dist.x > MAX_X / 2) {
-      dist.x -= MAX_X;
-    } else if (dist.x < -MAX_X / 2) {
-      dist.x += MAX_X;
-    }
-    if (dist.y > MAX_Y / 2) {
-      dist.y -= MAX_Y;
-    } else if (dist.y < -MAX_Y / 2) {
-      dist.y += MAX_Y;
-    }
-
-    double mag = dist.norm();
-    Vector dir = dist.normalized();
-
-    if (mag < SEGREGATION_RADIUS && mag > 0) {
-      double strength = (SEGREGATION_RADIUS - mag) / SEGREGATION_RADIUS;
-      if (strength < 0.0) {
-        strength = 0.0;
-      } else if (strength > 1.0) {
-        strength = 1.0;
-      }
-
-      TotalSegregation += dir * (MAX_SEGREGATION_FORCE * strength);
-    }
-
-    if (vel.normalized() * dir < fov_angle) {
+    if (dir * vel.normalized() < fov_angle || dist > COHENSION_RADIUS) {
       continue;
     }
+    sum += Boid_List[i]->pos;
+    count++;
+  }
 
-    if (mag < AGGREGATION_RADIUS && mag > 0) {
-      aggregationCentre += dist;
-      aggCount++;
+  if (count > 0) {
+    sum = sum * (1.0 / count); // Average of positions
+    Vector target = sum - pos;
+
+    target.setMag(MAX_VEL);
+
+    Vector diff = target - vel;
+    diff.setMag(MAX_COHESION_FORCE);
+    return diff;
+  } else {
+    return ZeroVec;
+  }
+}
+
+Vector Boid::Separation(Boid** Boid_List, int n)
+{
+  Vector sum(0, 0);
+  for (int i = 0; i < n; i++) {
+    Vector diff = Boid_List[i]->pos - pos;
+    double dist = diff.norm();
+    Vector dir = diff.normalized();
+
+    if (dist > SEPARATION_RADIUS) {
+      continue;
     }
-    if (mag < ALIGNMENT_RADIUS && mag > 0) {
-      alignmentCentre += Boid_List[i]->vel;
-      alignCount++;
+
+    double strength = (MAX_SEPARATION_FORCE - dist) / (MAX_SEPARATION_FORCE);
+    std::clamp(strength, 0.0, 1.0);
+    sum += dir * MAX_SEPARATION_FORCE * strength;
+  }
+  return sum;
+}
+
+Vector Boid::Alignement(Boid** Boid_List, int n)
+{
+  Vector sum(0, 0);
+  int count = 0;
+  double fov_angle = cos(FOV * 0.5);
+
+  for (int i = 0; i < n; i++) {
+    Vector diff = Boid_List[i]->pos - pos;
+    double dist = diff.norm();
+    Vector dir = diff.normalized();
+
+    if (dir * vel.normalized() < fov_angle || dist > ALIGNMENT_RADIUS) {
+      continue;
     }
+    sum += Boid_List[i]->vel - vel;
+    count++;
   }
 
-  Vector centre = vel;
-  centre.setMag(WANDER_DIST);
-  centre += pos;
-
-  Vector rotation = vel;
-  rotation.setMag(WANDER_RADIUS);
-  rotation = rotation.rotate(steeringAngle);
-  centre += rotation;
-  steer += (centre - pos);
-
-  steeringAngle += MAX_RANDOM_STEER * WanderAngleGen(boids_gen);
-
-  if (aggCount > 0) {
-    aggregationCentre = aggregationCentre * (1.0 / aggCount);
-    TotalAggregation += (aggregationCentre).normalized() * MAX_AGGREGATION_FORCE;
+  if (count > 0) {
+    sum = sum * (1.0 / count);
+    Vector diff = sum - vel;
+    diff.setMag(MAX_ALIGNMENT_FORCE);
+    return diff;
+  } else {
+    return ZeroVec;
   }
-  if (alignCount > 0) {
-    alignmentCentre = alignmentCentre * (1.0 / alignCount);
-    TotalAlignment += (alignmentCentre - vel).normalized() * MAX_ALIGNMENT_FORCE;
-  }
+}
 
-  steer += AGGREGATION_W * TotalAggregation;
-  steer -= SEGREGATION_W * TotalSegregation;
-  steer += ALIGNMENT_W * TotalAlignment;
+Vector Boid::ObjectAvoidance(Object** Object_List, int n)
+{
+
+  Vector sum(0, 0);
 
   Ray r1, r2, r3;
   r1.origin = pos + vel.normalized() * 8;
@@ -110,18 +110,18 @@ void Boid::update(double deltaTime, Boid** Boid_List, int n, Object** Objects, i
   RayHit hit;
   hit.dist = MAXFLOAT;
   RayHit testHit;
-  for (int i = 0; i < n_obj; i++) {
-    if (Objects[i]->rayCastCheck(r1, testHit)) {
+  for (int i = 0; i < n; i++) {
+    if (Object_List[i]->rayCastCheck(r1, testHit)) {
       if (testHit.dist < hit.dist) {
         hit = testHit;
       }
     }
-    if (Objects[i]->rayCastCheck(r2, testHit)) {
+    if (Object_List[i]->rayCastCheck(r2, testHit)) {
       if (testHit.dist < hit.dist) {
         hit = testHit;
       }
     }
-    if (Objects[i]->rayCastCheck(r3, testHit)) {
+    if (Object_List[i]->rayCastCheck(r3, testHit)) {
       if (testHit.dist < hit.dist) {
         hit = testHit;
       }
@@ -129,11 +129,45 @@ void Boid::update(double deltaTime, Boid** Boid_List, int n, Object** Objects, i
   }
 
   if (hit.dist <= AVOIDANCE_RADIUS) {
-    steer += hit.normal * (MAX_AVOIDANCE_FORCE * 2) * ((AVOIDANCE_RADIUS - hit.dist) / AVOIDANCE_RADIUS);
+    sum += hit.normal * (MAX_AVOIDANCE_FORCE * 2) * ((AVOIDANCE_RADIUS - hit.dist) / AVOIDANCE_RADIUS);
     // hit.Draw(renderer);
   }
+  return sum;
+}
 
-  vel += steer * deltaTime;
+Vector Boid::Wandering()
+{
+  Vector circle_centre = vel;
+  circle_centre.setMag(WANDER_DIST);
+  circle_centre += pos;
+
+  Vector rotation = vel;
+  rotation.setMag(WANDER_RADIUS);
+  rotation = rotation.rotate(steeringAngle);
+  circle_centre += rotation;
+
+  Vector target = (circle_centre - pos).normalized() * vel.norm();
+
+  steeringAngle += MAX_RANDOM_STEER * WanderAngleGen(boids_gen);
+  return target;
+}
+
+void Boid::update(double deltaTime, Boid** Boid_List, int n, Object** Objects, int n_obj, SDL_Renderer* renderer)
+{
+  Vector TotalSteer(0, 0);
+
+  Vector CohesionSteer = Cohesion(Boid_List, n);
+  Vector SeparationSteer = Separation(Boid_List, n);
+  Vector AlignmentSteer = Alignement(Boid_List, n);
+  Vector AvoidanceSteer = ObjectAvoidance(Objects, n_obj);
+
+  TotalSteer += CohesionSteer;
+  TotalSteer -= SeparationSteer;
+  TotalSteer += AlignmentSteer;
+  TotalSteer += Wandering();
+  TotalSteer += AvoidanceSteer;
+
+  vel += TotalSteer * deltaTime;
 
   if (vel.norm() > this->MAX_VEL) {
     vel.setMag(this->MAX_VEL);
